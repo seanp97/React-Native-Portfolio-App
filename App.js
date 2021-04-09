69bf203d50ef1ed288e51e4e870650076b6bf914
seanp97 <sean.pelser97@gmail.com>
2021-04-09 11:37:22 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Screen from './components/Screens';
+
+export default Screen;
